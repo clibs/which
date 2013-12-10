db94827577871c40681c3c0db821e96003266bb2
TJ Holowaychuk <tj@vision-media.ca>
2013-12-09 17:32:00 -0800

    ocd

--- test.c
+++ test.c
@@ -10,9 +10,11 @@ main() {
     it("should return NULL when no matching binary is found", {
       assert(NULL == which("foobar"));
     });
+
     it("should return the path of the name", {
       assert_str_equal("./test", which_path("test", mock_path));
     });
+
     it("should return NULL if the given name isn't executable", {
       assert(NULL == which_path("test.c", mock_path));
     });
